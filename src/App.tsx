501252f7435eeab1ac1a314f8d50b41fbd9f2195
X-Irrelevant-X <90459262+FardousNayeem@users.noreply.github.com>
2026-01-21 14:34:33 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Preloader from "./components/Pre";
-import Navbar from "./components/Navbar";
+import NavBar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
@@ -28,7 +28,7 @@ function App() {
     <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Navbar />
+        <NavBar />
         <ScrollToTop />
 
         <Routes>
